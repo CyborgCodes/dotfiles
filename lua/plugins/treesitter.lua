b1173ba5654e63bd0e54cbe936c9bd8a36ba1ef1
CyborgCodes <changtheng2@gmail.com>
2023-12-20 19:33:07 +0800

    treesitter lua added

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "javascript",
+        "tsx",
+        "lua",
+        "html",
+        "typescript",
+        "css",
+        "css",
+        "dart",
+      },
+
+      sync_install = false,
+
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+
+        additional_vim_regex_highlighting = false,
+      },
+      autotag = {
+        enable = true,
+      },
+    },
+  },
+}
